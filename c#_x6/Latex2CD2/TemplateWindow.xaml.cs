ddb56fd528bb68d86c20af5c1a2be84e39b0eeea
kleinsimon <klein@wtech.rub.de>
2013-08-09 08:50:42 +0200

    added .net revamp für X6

---
+++ c#_x6/Latex2CD2/TemplateWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Latex2CD2
+{
+    /// <summary>
+    /// Interaktionslogik für TemplateWindow.xaml
+    /// </summary>
+    public partial class TemplateWindow : Window
+    {
+        public TemplateWindow()
+        {
+            InitializeComponent();
+            TemplateText.Text = (Properties.Settings.Default.LastTemplate == string.Empty) ? Properties.Settings.Default.DefaultTemplate : Properties.Settings.Default.LastTemplate;
+        }
+
+        private void DefaultButton_Click(object sender, RoutedEventArgs e)
+        {
+            TemplateText.Text = Properties.Settings.Default.DefaultTemplate;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.LastTemplate = TemplateText.Text;
+
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+    }
+}
