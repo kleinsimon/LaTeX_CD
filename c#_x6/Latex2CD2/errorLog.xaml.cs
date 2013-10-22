4336cf6512cb2e5f41678d893e371acccd73faf0
kleinsimon <sklein8@googlemail.com>
2013-10-22 19:35:21 +0200

    fixed wrong template was saved with objects

---
+++ c#_x6/Latex2CD2/errorLog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Latex2CD2
+{
+    /// <summary>
+    /// Interaktionslogik für errorLog.xaml
+    /// </summary>
+    public partial class errorLog : Window
+    {
+        public string DisplayString
+        {
+            get { return OutputText.Text; }
+            set { OutputText.Text = value; }
+        }
+
+        public errorLog()
+        {
+            InitializeComponent();
+        }
+
+        public errorLog(string DisplayString)
+        {
+            InitializeComponent();
+            this.DisplayString = DisplayString;
+            this.Show();
+        }
+    }
+}
